05abbd40de6db8910f59c6beeb9ccda3c199b99c
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-23 10:50:08 +0700

    change title

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -18,7 +18,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid px-4">
-            <a class="navbar-brand font-weight-bold" href="/home">ASIKMEN</a>
+            <a class="navbar-brand font-weight-bold" href="/home">MENTAL</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
