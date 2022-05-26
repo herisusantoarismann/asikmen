5314f4904388865670656a96f72ca6869cd70f27
herisusantoarismann <heri.s.arisman@gmail.com>
2022-05-26 10:26:32 +0700

    Show error validasion message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,17 +21,26 @@
                 <b>Opps!</b> {{session('error')}}
             </div>
             @endif
+            @if(count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="card">
                 <div class="card-body">
                     <form action="{{ route('postLogin') }}" method="post">
                     @csrf
                         <div class="form-group">
                             <label>Email</label>
-                            <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                            <input type="email" name="email" class="form-control" placeholder="Email">
                         </div>
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                            <input type="password" name="password" class="form-control" placeholder="Password">
                         </div>
                         <button type="submit" class="btn btn-primary btn-block">Log In</button>
 
